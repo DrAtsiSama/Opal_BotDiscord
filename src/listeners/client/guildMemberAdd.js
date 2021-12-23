9481b1a0db2ccb20a450cfde312e28d02dd69014
Dareka <42326650+DarekaSama@users.noreply.github.com>
2021-12-23 21:46:47 +0100

    Ping / message bienvenue

--- src/listeners/client/guildMemberAdd.js
+++ src/listeners/client/guildMemberAdd.js
@@ -1,4 +1,5 @@
 const { Listener } = require('discord-akairo');
+const { cp } = require('fs');
 class GuildMemberAddListener extends Listener {
     constructor() {
         super('guildMemberAdd', {
@@ -6,8 +7,15 @@ class GuildMemberAddListener extends Listener {
             event: 'guildMemberAdd'
         });
     }
-    exec(member) {
-        console.log(`Bonjour, je suis ${member.user.username} !`);
+    async exec(member) {
+        const logChannel = this.client.channels.cache.get('805091401900752936');
+        const embed = this.client.functions.embed()
+            .setDescription(`**${member.user.tag}** à rejoint le serveur.`)
+            .setFooter(`Members : ${member.guild.memberCount.toLocaleString()}`)
+            .setColor('#42d6ff')
+        await logChannel.send({ embeds: [embed] })
+            .then(() => console.log(`guildMemberAdd -> Message envoyé pour ${member.user.tag}.`))
+            .catch(() => console.log(`guildMemberAdd -> Le massage n'a pas été envoyé pour ${member.user.tag}.`))
     }
 }
 module.exports = GuildMemberAddListener;
